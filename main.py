f71f0e91d5a29e22db135e435f7605d5690fb31a
rushinolk <arthurmachado7191@hotmail.com>
2025-05-20 07:20:22 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Dic está aprontando")
+
+@bot.event
+async def on_member_join(membro:discord.Member):
+    canal = bot.get_channel(1372981097800929333)
+    await canal.send(f"{membro.mention} entrou no servidor, acabou a farra")
+
+@bot.event
+async def on_member_remove(membro:discord.Member):
+    canal = bot.get_channel(1372985552114155530)
+    await canal.send(f"{membro.mention} foi eliminado")
+
+# ctx é o contexto em que a funação é chamada abrangendo todos os dados do evento
+@bot.command()
+async def ola(ctx:commands.Context):
+    nome = ctx.author.mention
+    await ctx.send(f"Koe {nome}, tranquilidade?")
+
+
+# Manter no final
+bot.run("MTM3Mjk3MDc5MTQxNDc5NjI4OA.GL8-3I.qQPCIwXo_lDJKvXGlmBJ9ZSVPG-g3FeLlG92Uo") 
